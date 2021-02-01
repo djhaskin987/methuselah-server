fec1072b68307ab1ca1c899d0f8e3bf7796d13c7
Daniel Jay Haskin <djhaskin987@gmail.com>
2021-01-31 17:18:01 -0700

    Trying to get this server off the ground

---
+++ src/test/java/io/github/djhaskin987/methuselah/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.djhaskin987.methuselah.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
