1489d596725970577dd5cff6419367ce983c20ac
Daniel Jay Haskin <djhaskin987@gmail.com>
2021-02-06 08:57:00 -0700

    Add a basic Capture.java, more to follow

---
+++ src/main/java/io/github/djhaskin987/methuselah/server/Capture.java
@@ -0,0 +1,56 @@
+package io.github.djhaskin987.methuselah.server;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ * Represents a capture in the database.
+ */
+@Entity
+@Table(name = "captures")
+@EntityListeners(AuditingEntityListener.class)
+public class Capture {
+
+    /**
+     * Primary key.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    /**
+     * Content address, which is usually the sha512 hash of the object/file.
+     */
+    @Column(name = "content_address", nullable = false)
+    private String contentAddress;
+
+    /**
+     * Filename for when the file is re-hydrated. it gets put here.
+     */
+    @Column(name = "file_address", nullable = false)
+    private String fileAddress;
+
+    /**
+     * Getter for content address.
+     *
+     * @return content address.
+     */
+    public String getContentAddress() {
+        return contentAddress;
+    }
+
+    /**
+     * Getter for file address.
+     *
+     * @return file address.
+     */
+    public String getFileAddress() {
+        return fileAddress;
+    }
+}
