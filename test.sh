cf1ac0bdaeb169a20ffed5439bf393d8a669b218
Daniel Jay Haskin <djhaskin987@gmail.com>
2021-02-15 20:06:01 -0700

    Finish MVP of content controller

--- test.sh
+++ test.sh
@@ -2,11 +2,9 @@ set -ex
 
 
 content_address="$(sha256sum eclipse-formatter.xml | awk '//{print $1}')"
-curl -v --head http://localhost:8080/api/v1/objects/abc
-curl -v --head 
-    http://localhost:8080/api/v1/objects/${content_address}
-
+curl -v --head http://localhost:8080/api/v1/object/abc
+curl -v --head  \
+    http://localhost:8080/api/v1/object/${content_address}
 
 curl -v -X PUT \
-    -F "content=@eclipse-formatter.xml" http://localhost:8080/api/v1/objects/${content_address}
-
+    -F "content=@eclipse-formatter.xml" http://localhost:8080/api/v1/object/${content_address}
